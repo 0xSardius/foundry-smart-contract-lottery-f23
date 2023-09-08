a0e377ddd6dd77321dfb0c0b143cbec145d95576
Sardius <0xsardius@gmail.com>
2023-09-07 21:17:52 -0400

    gm

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,6 +2,9 @@
 
 pragma solidity 0.8.18;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+
 /**
  * @title Sample Raffle Contract
  * @author Sardius
@@ -9,25 +12,24 @@ pragma solidity 0.8.18;
  * @dev Implements Chainlink VRF2
  */
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     // Custom errors are more gas efficient than standard revert
     /**Error naming convention
      * Prefix the error with the name of the contract
      */
     error Raffle__NotEnoughEthSent();
-    
+
     // State Variables
-    uint256 private constant REQUEST_CONFIRMATIONS = 3;
-    uint256 private constant NUM_WORDS = 1;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
-    uint256 private immutable = i_entranceFee;
+    uint256 private immutable i_entranceFee;
     // @dev Duration of the lottery in seconds
-    uint256 private immutable = i_interval;
-    address private immutable = i_vrfCoordinator;
-    bytes32 private immutable = i_gasLane;
-    uint64 private immutable = i_subscriptionId;
-    uint32 private immutable = i_callbackGasLimit;
-
+    uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
 
     // Payable dynamic array allows us to pay out the winner
     address payable[] private s_players;
@@ -35,47 +37,56 @@ contract Raffle {
 
     // Events
     event EnteredRaffle(address indexed player, uint256 indexed entranceFee);
-    
 
-    constructor(uint256 entranceFee, 
-    uint256 interval, address vrfCoordinator, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit, uint8 numWords) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscription Id,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
-        i_vrfCoordinator = vrfCoordinator;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
-        s_lastTimeStamp = block.timestamp; 
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Not enough ETH sent");\
         if (msg.value < i_entranceFee) {
-            revert NotEnoughEthSent();
+            revert Raffle__NotEnoughEthSent();
         }
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender, msg.value);
-
-    }   
+    }
 
     //1. Get a random number
     //2. Use a random number to pick a player
     //3. Be automatically called
     function pickWinner() external {
         // Check to see if enough time has passed
-        if ((block.timestamp - lastTimestamp) < i_interval) {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
         // 1. Request the RNG
         // 2. Get the random number
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
-            keyHash, //gasLane
-            s_subscriptionId, //ID you've funded with link
+            i_gasLane, //gasLane
+            i_subscriptionId, //ID you've funded with link
             REQUEST_CONFIRMATIONS,
-            callbackGasLimit,
+            i_callbackGasLimit,
             NUM_WORDS
         );
     }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
 
     /** Getter Functions */
 
