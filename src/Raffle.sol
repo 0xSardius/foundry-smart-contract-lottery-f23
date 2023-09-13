cbe2e21eb2a23fb2aa7e8d139182948657d6ec54
Sardius <0xsardius@gmail.com>
2023-09-12 21:24:27 -0400

    Adding Chainlink automation functionality

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -20,6 +20,7 @@ contract Raffle is VRFConsumerBaseV2 {
     error Raffle__NotEnoughEthSent();
     error Raffle__TransferFailed();
     error Raffle__RaffleNotOpen();
+    error Raffle__UpkeepNotNeeded();
 
     // Type Declarations
     enum RaffleState {
@@ -80,14 +81,39 @@ contract Raffle is VRFConsumerBaseV2 {
         emit EnteredRaffle(msg.sender, msg.value);
     }
 
+    // When is the winner supposed to be picked?
+    /**
+     * @dev This is the function that the Chainlink Automation nodes call
+     * to see if it's time to perform an upkeep
+     * The following should be true for this to return true:
+     * 1. The time interval has passed between raffle runs
+     * 2. The raffle is in the OPEN state
+     * 3. The contract has ETH (aka, players)
+     * 4. (Implicit) The subscription is funded with LINK
+     */
+    function checkUpkeep(bytes memory /* checkData */) public view returns (bool upkeepNeeded, bytes memory /* performData */) {
+        bool timeHasPassed = (block.timestamp - s_lastTimeStamp) >= i_interval;
+        bool isOpen = RaffleState.OPEN == s_raffleState;
+        bool hasBalance = address(this).balance > 0;
+        bool hasPlayers = s_players.length > 0;
+        upkeepNeeded = (timeHasPassed && isOpen && hasBalance && hasPlayers);
+        return (upkeepNeeded, "0x0");
+    }
+
     //1. Get a random number
     //2. Use a random number to pick a player
     //3. Be automatically called
-    function pickWinner() external {
-        // Check to see if enough time has passed
-        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
-            revert();
+    function performUpkeep(bytes calldata /* performData */) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Raffle__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(S_raffleState)
+            );
         }
+        // Check to see if enough time has passed
+        
         s_raffleState = RaffleState.CALCULATING;
         // 1. Request the RNG
         // 2. Get the random number
