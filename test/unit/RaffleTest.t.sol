abb4a6d3dad70543a23fac0a070eb7ec8d08e399
Sardius <0xsardius@gmail.com>
2023-09-16 14:52:35 -0400

    Building helper config, deploy and tests

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT;
+
+pragma solidity ^0.8.18;
+
+import {DeployRaffle} from "./DeployRaffle.s.sol";
+import { Raffle } from "../../src/Raffle.sol;
+import {Test, console} from "forge-std/Test.sol";
+
+contract RaffleTest {
+
+}
